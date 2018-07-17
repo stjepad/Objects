ae090e289f3aaa3b6876daa72c0a5acd54936fea
Dejan Stjepanovic <ds2q88@gmail.com>
2018-07-17 16:02:47 -0500

    practice done

---
+++ main.js
@@ -0,0 +1,77 @@
+console.log("meow")
+
+// Elizabeth Sanger's campaign info
+const congressionalDistrict = {
+    districtNumber: "1"
+}
+
+const platform = {
+    taxes: "Save the Rich",
+    jobs: "$5 minimum wage",
+    infrastructure: "Infra-wha?",
+    healthCare: "Mine is free",
+    CrimeAndEnforecement: "Lock her up!"
+}
+
+const donationForm = {
+    donationURL: "https://secure.donaldjtrump.com/we-made-history-now?utm_campaign=ad_donate_djt1201084755&utm_medium=ad&utm_source=djt_google_search&utm_content=t1_ad5_d&gclid=CjwKCAjwp7baBRBIEiwAPtjwxKAEwcqXGhTorgGWeszWsCZJgQ_huGjo0ALcMA0sKIlXmlbvpQMI7BoCy4IQAvD_BwE"
+}
+
+const calendar = [
+    {
+        eventName: "Bloop",
+        eventDate: "01/23/2018",
+        eventStartTime: "7:00PM",
+    },
+    {
+        eventName: "Blaap",
+        eventDate: "08/23/2018",
+        eventStartTime: "12:00PM",
+    },
+    {
+        eventName: "Zip",
+        eventDate: "04/20/2018",
+        eventStartTime: "08:00AM",
+    },
+]
+
+const volunteerInfo = [
+    {
+        volName: "sdf",
+        volAddress: "sfdsdf",
+        volEmail: "sfsdf",
+        volPhone: "sdfdsfdf",
+        volAvailability: [
+            {
+                date: "kjhu",
+                time: "edpfki"
+            },
+            {
+                date: "peofu",
+                time: "nfnfn"
+            },
+            {
+                date: "20d8",
+                time: "dmdkdju"
+            },
+        ]
+    }
+]
+
+const biography = {
+    content: "kfdiusdfiluhiuhifdaliuhfsliu"
+}
+
+const images = {
+    headShot: "",
+    FamilyPic: "",
+    ConstituentsPic: ""
+}
+
+const missionStatement = {
+    content: ""
+}
+
+const voterRegistration = {
+    url: ""
+}
